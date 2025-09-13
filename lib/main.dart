0294eabbdbcf6892ed0d8e0e42d588233443a382
179292160 <Rei-san001@users.noreply.github.com>
2025-09-14 01:46:58 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //const is used to optimize runtime optimization
+  runApp(
+      const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    );
+  }
---
+++ lib/tempCodeRunnerFile.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  //const is used to optimize runtime optimization
+  runApp(const MaterialApp(home: Text('Hello world')));
+}
