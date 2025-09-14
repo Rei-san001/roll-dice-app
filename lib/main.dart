e604dc2887de7e83ea5aaddede214c41e43ab7cd
179292160 <Rei-san001@users.noreply.github.com>
2025-09-15 01:41:25 +0600

    classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
 
 void main() {
   //const is used to optimize runtime optimization
   runApp(
-      MaterialApp(
-        home: Scaffold(
-          //backgroundColor: Color.fromARGB(255, 255, 255, 1),
-          //backgroundColor: Colors.indigoAccent,
-          body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 26, 2, 80),
-                  Color.fromARGB(255, 152, 140, 213),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const Center(
-              child: Text('Hello world', style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-              ),
-              ),
-            ),
-          ),
-        ),
+    MaterialApp(
+      home: Scaffold(
+        //backgroundColor: Color.fromARGB(255, 255, 255, 1),
+        //backgroundColor: Colors.indigoAccent,
+        body: GradientContainer(),
       ),
-    );
-  }
+    ),
+  );
+}
+
+
