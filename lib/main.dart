381bb662b039c5d4e52250b139b513acab87dd3f
179292160 <Rei-san001@users.noreply.github.com>
2025-09-15 00:54:14 +0600

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   //const is used to optimize runtime optimization
   runApp(
-      const MaterialApp(
+      MaterialApp(
         home: Scaffold(
-          body: Center(
-            child: Text('Hello World!'),
+          //backgroundColor: Color.fromARGB(255, 255, 255, 1),
+          //backgroundColor: Colors.indigoAccent,
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 26, 2, 80),
+                  Color.fromARGB(255, 152, 140, 213),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const Center(
+              child: Text('Hello world', style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+              ),
+              ),
+            ),
           ),
         ),
       ),
--- lib/tempCodeRunnerFile.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main(){
-  //const is used to optimize runtime optimization
-  runApp(const MaterialApp(home: Text('Hello world')));
-}
